a8cc53aecbc3ce9cb8f5c6565897f60211c06673
Adarsh <nulltontechnologies@gmail.com>
2020-05-02 17:26:34 +0530

    added mutation

--- graph-ql/resolvers.js
+++ graph-ql/resolvers.js
@@ -4,6 +4,17 @@ const resolvers = {
     Query: {
         people: () => people,
         person: (_, {id}) => (people.filter((person) => person.id === id))[0]
+    },
+    Mutation: {
+        addPerson: (_, {name, age}) => {
+            const person = {
+                id: people.length + 1,
+                name,
+                age
+            }
+            people.push(person)
+            return person
+        }
     }
 }
 
